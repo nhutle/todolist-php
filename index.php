73d71f7ba05df51f4d21b8cc831e51d6f5099ece
Nhut Le <leminhnhut08t1@gmail.com>
2020-03-26 22:36:38 +0700

    call proper function of controller based on ?action

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 include_once("app/controllers/WorksController.php");
 
+$action = !empty($_REQUEST['action']) ? $_REQUEST['action'] : "index";
+
 $workController = new WorksController();
-$workController->invoke();
+$workController->$action();
