8fc17620fbb73c2bedc943ba31b9f73662778b31
Nhut Le <leminhnhut08t1@gmail.com>
2020-03-27 22:46:21 +0700

    finalize unit test for work controller

--- WorksControllerTest.php
+++ WorksControllerTest.php
@@ -16,13 +16,51 @@ class WorksControllerTest extends TestCase
 
     public function testAddWork()
     {
-        $_POST = array(
+        $newWork = array(
             'name'         => 'Test_Add_Work',
-            'startingDate' => '2020-03-25',
-            'endingDate'   => '2020-03-27',
-            'status'       => 'Doing'
+            'startingDate' => '2020-04-01',
+            'endingDate'   => '2020-04-03',
+            'status'       => 'Planning'
         );
-        $message = $this->workController->addWork();
-        $this->assertEquals('New work has been successfully added', $message['success']);
+        $result = $this->workController->addWork($newWork);
+        $this->assertEquals('New work has been successfully added', $result['success']);
+    }
+
+    public function testUpdateWork()
+    {
+        $workByName = $this->workController->getWorkByName('Test_Add_Work');
+
+        if ($workByName) {
+            $workToUpdate = array(
+                'id'           => $workByName['id'],
+                'name'         => 'Test_Add_Work_Updated',
+                'startingDate' => $workByName['starting_date'],
+                'endingDate'   => $workByName['ending_date'],
+                'status'       => $workByName['status']
+            );
+            $result = $this->workController->updateWork($workToUpdate);
+
+            if (isset($result['success'])) {
+                $updatedWork = $this->workController->getWork($workByName['id']);
+                $this->assertEquals($updatedWork['name'], 'Test_Add_Work_Updated');
+            } else {
+                $this->assertEquals('Fail to save, an unknown error occurred', $result['error']);
+            }
+        }
+    }
+
+    public function testDeleteWork()
+    {
+        $workByName = $this->workController->getWorkByName('Test_Add_Work_Updated');
+
+        if ($workByName) {
+            $result = $this->workController->deleteWork($workByName['id']);
+
+            if (isset($result['success'])) {
+                $this->assertEquals('The work has been successfully removed', $result['success']);
+            } else {
+                $this->assertEquals('Fail to delete, an unknown error occurred', $result['error']);
+            }
+        }
     }
 }
